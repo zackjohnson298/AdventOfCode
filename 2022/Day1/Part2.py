c17d89007cf09a746343cb387f652df1f0d80e1e
zackjohnson298 <zackjohnson298@gmail.com>
2022-11-30 22:08:24 -0700

    2022 Day 1 complete

---
+++ 2022/Day1/Part2.py
@@ -0,0 +1,22 @@
+
+def get_input(filename):
+    with open(filename) as file:
+        lines = file.read().splitlines()
+    output = []
+    current = []
+    for line in lines:
+        if len(line) == 0:
+            output.append(current)
+            current = []
+        else:
+            current.append(int(line))
+    output.append(current)
+    return output
+
+
+def main():
+    data = get_input('input.txt')
+    print(sum(sorted([sum(group) for group in data])[-3:]))
+
+
+main()
