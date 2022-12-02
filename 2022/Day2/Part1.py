99add03dc6bfe6c7d3c207cef71ce82f3231f7f2
zackjohnson298 <zackjohnson298@gmail.com>
2022-12-01 22:31:54 -0700

    Day2 Complete

--- 2022/Day2/Part1.py
+++ 2022/Day2/Part1.py
@@ -3,21 +3,53 @@
 def get_input(filename):
     with open(filename) as file:
         lines = file.read().splitlines()
-    output = []
-    current = []
+    your_moves = []
+    their_moves = []
     for line in lines:
-        if len(line) == 0:
-            output.append(current)
-            current = []
-        else:
-            current.append(int(line))
-    output.append(current)
-    return output
+        them, you = line.split()
+        your_moves.append(you)
+        their_moves.append(them)
+    return their_moves, your_moves
+
+
+def evaluate(their_move, your_move):
+    points = {
+        'X': 1,
+        'Y': 2,
+        'Z': 3
+    }
+    outcome = {
+        'X': {
+            'win': 'C',
+            'loss': 'B'
+        },
+        'Y': {
+            'win': 'A',
+            'loss': 'C'
+        },
+        'Z': {
+            'win': 'B',
+            'loss': 'A'
+        }
+    }
+    score = points[your_move]
+    if their_move == outcome[your_move]['win']:
+        score += 6
+    elif their_move != outcome[your_move]['loss']:
+        score += 3
+    return score
 
 
 def main():
-    data = get_input('input.txt')
-    print(max([sum(group) for group in data]))
-    
+    their_moves, your_moves = get_input('input.txt')
+    total = 0
+    for their_move, your_move in zip(their_moves, your_moves):
+        score = evaluate(their_move, your_move)
+        print(their_move, your_move, score)
+        total += score
+    print()
+    print(total)
+
 
 main()
+
--- 2022/Day2/Part2.py
+++ 2022/Day2/Part2.py
@@ -1,22 +1,58 @@
 
+
 def get_input(filename):
     with open(filename) as file:
         lines = file.read().splitlines()
-    output = []
-    current = []
+    outcomes = []
+    their_moves = []
     for line in lines:
-        if len(line) == 0:
-            output.append(current)
-            current = []
-        else:
-            current.append(int(line))
-    output.append(current)
-    return output
+        them, you = line.split()
+        outcomes.append(you)
+        their_moves.append(them)
+    return their_moves, outcomes
+
+
+def evaluate(their_move, desired_outcome):
+    points = {
+        'A': 1,     # Rock
+        'B': 2,     # Paper
+        'C': 3      # Scissors
+    }
+    possibilities = {
+        'A': {
+            'X': 'C',       # Loss
+            'Y': 'A',       # Draw
+            'Z': 'B'        # Win
+        },
+        'B': {
+            'X': 'A',
+            'Y': 'B',
+            'Z': 'C'
+        },
+        'C': {
+            'X': 'B',
+            'Y': 'C',
+            'Z': 'A'
+        }
+    }
+    your_move = possibilities[their_move][desired_outcome]
+    score = points[your_move]
+    if desired_outcome == 'Z':      # Win
+        score += 6
+    elif desired_outcome == 'Y':    # Draw
+        score += 3
+    return score
 
 
 def main():
-    data = get_input('input.txt')
-    print(sum(sorted([sum(group) for group in data])[-3:]))
+    their_moves, outcomes = get_input('input.txt')
+    total = 0
+    for their_move, desired_outcome in zip(their_moves, outcomes):
+        score = evaluate(their_move, desired_outcome)
+        print(their_move, desired_outcome, score)
+        total += score
+    print()
+    print(total)
 
 
 main()
