d8b091875f8baa88677c54fe8f5245454daa68af
zackjohnson298 <zackjohnson298@gmail.com>
2022-12-02 22:26:01 -0700

    Setup Day 4

---
+++ 2022/Day4/Part1.py
@@ -0,0 +1,27 @@
+
+def get_input(filename):
+    with open(filename) as file:
+        lines = file.read().splitlines()
+    compartment_1 = [set(line[:int(len(line) / 2)]) for line in lines]
+    compartment_2 = [set(line[int(len(line) / 2):]) for line in lines]
+    rucksacks = zip(compartment_1, compartment_2)
+    return rucksacks
+
+
+def get_score(letter: str):
+    if letter.islower():
+        return ord(letter) - ord('a') + 1
+    else:
+        return ord(letter) - ord('A') + 27
+
+
+def main():
+    rucksacks = get_input('input.txt')
+    total = 0
+    for compartment_1, compartment_2 in rucksacks:
+        shared = list(compartment_1.intersection(compartment_2))[0]
+        total += get_score(shared)
+    print(total)
+
+
+main()
---
+++ 2022/Day4/Part2.py
@@ -0,0 +1,29 @@
+
+
+def get_input(filename):
+    with open(filename) as file:
+        lines = file.read().splitlines()
+    groups = []
+    for ii in range(0, len(lines), 3):
+        group = [set(line) for line in lines[ii:ii + 3]]
+        groups.append(group)
+    return groups
+
+
+def get_score(letter: str):
+    if letter.islower():
+        return ord(letter) - ord('a') + 1
+    else:
+        return ord(letter) - ord('A') + 27
+
+
+def main():
+    groups = get_input('input.txt')
+    total = 0
+    for a, b, c in groups:
+        shared = list(a.intersection(b).intersection(c))[0]
+        total += get_score(shared)
+    print(total)
+
+
+main()
