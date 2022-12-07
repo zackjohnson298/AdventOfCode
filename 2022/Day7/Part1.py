a3659ec0c8b930a873797bc3759cb33b66e8e9ff
zackjohnson298 <zackjohnson298@gmail.com>
2022-12-06 23:12:39 -0700

    Doesn't work wtf

--- 2022/Day7/Part1.py
+++ 2022/Day7/Part1.py
@@ -1,18 +1,57 @@
+import json
+
 
 def get_input(filename):
     with open(filename) as file:
         lines = file.read().splitlines()
-    return lines
+    tree = {'/': {}}
+    current_path = ['/']
+    state = 'adding files'
+    for line in lines[2:]:
+        if '$' in line:
+            if ' cd ' in line:      # This is suspicious. If I check for ls instead it no longer works (key error at vmvpf)
+                if '..' in line:
+                    current_path.pop()
+                else:
+                    current_path.append(line.split()[-1])
+        else:
+            cwd = tree
+            for directory in current_path:
+                cwd = cwd[directory]
+            type_or_size, name = line.split()
+            if type_or_size == 'dir' and name not in cwd:
+                cwd[name] = {}
+            else:
+                cwd[name] = int(type_or_size)
+
+    return tree
+
+
+def get_size(tree, sizes):
+    total = 0
+    for name, value in tree.items():
+        if type(value) == int:
+            total += value
+        else:
+            new_value = get_size(value, sizes)
+            total += new_value
+            if name in sizes:
+                sizes[name] += new_value
+            else:
+                sizes[name] = new_value
+    return total
 
 
 def main():
-    lines = get_input('input.txt')
-    length = 4
-    for line in lines:
-        for ii in range(length, len(line)):
-            if len(set(line[ii-length:ii])) == length:
-                print(ii)
-                break
-
+    tree = get_input('input.txt')
+    print(json.dumps(tree, indent=4))
+    sizes = {'/': 0}
+    get_size(tree, sizes)
+    print(json.dumps(sizes, indent=4))
+    total = 0
+    for key, value in sizes.items():
+        if value <= 100000:
+            total += value
+    print(total)
 
 main()
