307bbbc44d4cea929c88e6c7a2005e92a1af540a
zackjohnson298 <zackjohnson298@gmail.com>
2022-12-12 22:06:35 -0700

    Setup Day 13

---
+++ 2022/Day13/Part1.py
@@ -0,0 +1,14 @@
+import json
+
+
+def get_input(filename):
+    with open(filename) as file:
+        lines = file.read().splitlines()
+    return monkeys
+
+
+def main():
+    monkeys = get_input('test_input.txt')
+
+
+main()
