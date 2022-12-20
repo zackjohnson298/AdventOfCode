c7ab997642d2cfbac249cd54815d982041b0d4af
Zack Johnson <zjo@trackman.com>
2022-12-20 09:46:02 -0700

    Day 20 Complete

--- 2022/Day20/Part1.py
+++ 2022/Day20/Part1.py
@@ -1,6 +1,3 @@
-import json
-from random import choice
-
 
 def get_input(filename):
     with open(filename) as file:
@@ -12,64 +9,38 @@ def sign(number):
     return 1 if number > 0 else -1 if number < 0 else None
 
 
-def construct_numbers(codes) -> [int]:
-    numbers = [None] * len(codes)
+def find_code(code_id, codes):
     for code in codes:
-        index = code['index']
-        if numbers[index] is not None:
-            raise ValueError('invalid code list')
-        numbers[index] = code['value']
+        if code['id'] == code_id:
+            return code
+    return None
+
+
+def mix(numbers: [int]) -> [int]:
+    codes = [{'id': ii, 'value': v} for ii, v in enumerate(numbers)]
+    for number_id, value in enumerate(numbers):
+        code = find_code(number_id, codes)
+        if code is None:
+            print('Could not find code')
+            return
+        index = codes.index(code)
+        new_index = (index + value) % (len(codes) - 1)
+        _ = codes[new_index]
+        codes.pop(index)
+        codes.insert(new_index, code)
+    numbers = [code['value'] for code in codes]
     return numbers
 
 
 def main():
     numbers = get_input('input.txt')
-    codes = [{'value': value, 'index': index, 'id': index} for index, value in enumerate(numbers)]
-    # print(construct_numbers(codes))
-    # _ = input()
-    for ii, number in enumerate(numbers):
-        print(ii, len(numbers))
-        index = codes[ii]['index']
-        moved_code_id = codes[ii]['id']
-        upper_index = (number % len(codes)) + 1
-        if number >= 0:
-            codes[ii]['index'] = (codes[ii]['index'] + number) % len(codes)
-        else:
-            codes[ii]['index'] = (codes[ii]['index'] + number - 1) % len(codes)
-            upper_index = ((number - 1) % len(codes)) + 1
-        for jj in range(1, upper_index):
-            code_index = (index + jj) % len(codes)
-            code_to_move = {}
-            for code in codes:
-                if code['id'] == moved_code_id:
-                    continue
-                if code['index'] == code_index:
-                    code_to_move = code
-                    break
-            else:
-                print('Could not find code')
-                return
-            code_to_move['index'] -= 1
-        # print()
-        # print(ii+1)
-        # print(construct_numbers(codes))
-        # # print(json.dumps(codes, indent=4))
-        # _ = input('continue: ')
-    numbers = construct_numbers(codes)
+    numbers = mix(numbers)
     index = numbers.index(0)
     a = numbers[(index + 1000) % len(numbers)]
     b = numbers[(index + 2000) % len(numbers)]
     c = numbers[(index + 3000) % len(numbers)]
     print(a, b, c)
     print(a + b + c)
-
-
-
-
-
-
-
-
 
 
 main()
--- 2022/Day20/Part2.py
+++ 2022/Day20/Part2.py
@@ -1,102 +1,56 @@
-import json
-from random import choice, shuffle, uniform
-
 
 def get_input(filename):
     with open(filename) as file:
         lines = file.read().splitlines()
-    blueprints = {}
-    for ii, line in enumerate(lines, start=1):
-        line = line.split(': ')[1]
-        ore_str, clay_str, obs_str, geo_str = line.split('. ')
-        ore_cost_list = [int(char) for char in ore_str.split() if char.isdigit()]
-        clay_cost_list = [int(char) for char in clay_str.split() if char.isdigit()]
-        obs_cost_list = [int(char) for char in obs_str.split() if char.isdigit()]
-        geo_cost_list = [int(char) for char in geo_str.split() if char.isdigit()]
-        blueprint = {
-            'ore': {
-                'ore': ore_cost_list[0]
-            },
-            'clay': {
-                'ore': clay_cost_list[0]
-            },
-            'obs': {
-                'ore': obs_cost_list[0],
-                'clay': obs_cost_list[1]
-            },
-            'geo': {
-                'ore': geo_cost_list[0],
-                'obs': geo_cost_list[1]
-            }
-        }
-        blueprints[ii] = blueprint
-    return blueprints
+    return [int(value) for value in lines]
 
 
-def can_build(robot_type, inventory, blueprint):
-    cost = blueprint[robot_type]
-    for key, value in cost.items():
-        if inventory[key] < value:
-            return False
-    return True
+def sign(number):
+    return 1 if number > 0 else -1 if number < 0 else None
 
 
-def build_robot(robot_type, inventory, blueprint):
-    cost = blueprint[robot_type]
-    for key, value in cost.items():
-        inventory[key] -= value
+def find_code(code_id, codes):
+    for code in codes:
+        if code['id'] == code_id:
+            return code
+    return None
 
 
-def update_inventory(robots, inventory):
-    for key, value in robots.items():
-        inventory[key] += value
-
-
-def get_max_geodes(blueprint, minutes=24, iterations=20):
-    max_geodes = 0
-    count = 0
-    for ii in range(iterations):
-        geodes = simulate(blueprint, minutes)
-        if geodes == max_geodes:
-            count += 1
-        elif geodes > max_geodes:
-            max_geodes = geodes
-            count = 1
-    return max_geodes, count
-
-
-def simulate(blueprint: {}, minutes=24) -> int:
-    inventory = {'ore': 0, 'clay': 0, 'obs': 0, 'geo': 0}
-    robots = {'ore': 1, 'clay': 0, 'obs': 0, 'geo': 0}
-    prioritised_options = ['geo', 'obs', 'clay', 'ore']
-    shuffle(prioritised_options)
-    if uniform(0, 1) > 0.5:
-        prioritised_options.remove('geo')
-        prioritised_options.insert(0, 'geo')
-    for minute in range(minutes):
-        robots_being_built = {key: 0 for key in robots}
-        # Randomly prioritize a certain robot:
-        for robot in prioritised_options:
-            if can_build(robot, inventory, blueprint) and uniform(0, 1) > 0.05:
-                build_robot(robot, inventory, blueprint)
-                robots_being_built[robot] += 1
-                break
-        update_inventory(robots, inventory)
-        for key, value in robots_being_built.items():
-            robots[key] += value
-    return inventory['geo']
+def mix(numbers: [int], codes) -> [int]:
+    for number_id, value in enumerate(numbers):
+        # if value == 0:
+        #     continue
+        code = find_code(number_id, codes)
+        if code is None:
+            print('Could not find code')
+            return
+        index = codes.index(code)
+        new_index = (index + value) % (len(codes) - 1)
+        _ = codes[new_index]
+        codes.pop(index)
+        codes.insert(new_index, code)
+    numbers = [code['value'] for code in codes]
+    return numbers, codes
 
 
 def main():
-    blueprints = get_input('test_input.txt')
-    max_geodes_1, count = get_max_geodes(blueprints[1], minutes=32, iterations=100000)
-    print('1: ', max_geodes_1, count)
-    max_geodes_2, count = get_max_geodes(blueprints[2], minutes=32, iterations=100000)
-    print('2: ', max_geodes_2, count)
-    max_geodes_3, count = get_max_geodes(blueprints[3], minutes=32, iterations=100000)
-    print('3: ', max_geodes_3, count)
+    numbers = get_input('input.txt')
+    key = 811589153
+    numbers = [number*key for number in numbers]
+    codes = [{'id': ii, 'value': v} for ii, v in enumerate(numbers)]
+    new_numbers = numbers
+    for ii in range(10):
+        print(ii+1, '/', 10)
+        new_numbers, codes = mix(numbers, codes)
+
+    index = new_numbers.index(0)
+    a = new_numbers[(index + 1000) % len(numbers)]
+    b = new_numbers[(index + 2000) % len(numbers)]
+    c = new_numbers[(index + 3000) % len(numbers)]
     print()
-    print(max_geodes_1 * max_geodes_2 * max_geodes_3)
+    print(a, b, c)
+    print()
+    print(a + b + c)
 
 
 main()
