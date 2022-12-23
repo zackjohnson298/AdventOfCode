e35bb47cf46101d792ec7441922be2dd710acd9f
zackjohnson298 <zackjohnson298@gmail.com>
2022-12-22 17:49:27 -0700

    Part 2 still unfinished

--- 2022/Day22/Part2.py
+++ 2022/Day22/Part2.py
@@ -1,81 +1,330 @@
+import numpy as np
+
+
+def rot_z(angle):
+    angle = np.pi * angle / 180
+    return np.array([
+        [np.cos(angle), -np.sin(angle)],
+        [np.sin(angle), np.cos(angle)]
+    ], dtype='int')
+
+
+def get_steps(word):
+    steps = []
+    number_str = ''
+    for char in word:
+        if char.isdigit():
+            number_str = number_str + char
+        else:
+            steps.append(int(number_str))
+            steps.append(char)
+            number_str = ''
+    if number_str.isdigit():
+        steps.append(int(number_str))
+    return steps
+
 
 def get_input(filename):
     with open(filename) as file:
         lines = file.read().splitlines()
-    monkeys = {}
+    grid = []
+    steps = get_steps(lines.pop())
+    lines.pop()
+    max_width = max([len(line) for line in lines])
+
     for line in lines:
-        name, str_2 = line.split(': ')
-        if str_2.isdigit():
-            children = []
-            op = None
-            value = int(str_2)
-        else:
-            child1, op, child2 = str_2.split()
-            children = (child1, child2)
-            value = None
-        monkey = {
-            'name': name,
-            'value': value,
-            'op': op,
-            'children': children
+        row = []
+        line = line + ' '*(max_width - len(line))
+        for char in line:
+            if char == '.':
+                row.append(0)
+            elif char == '#':
+                row.append(1)
+            elif char == ' ':
+                row.append(2)
+        grid.append(row)
+    return np.array(grid, dtype='int'), steps
+
+
+def rotate_matrix_counterclockwise(m, count=1):
+    for _ in range(count):
+        m = [[m[j][i] for j in range(len(m))] for i in range(len(m[0])-1,-1,-1)]
+    return m
+
+
+''' Specific to Test Input '''
+def get_test_mapping():
+    size = 4
+    mapping = {
+        'facing': {
+            1: np.eye(2),
+            2: np.eye(2),
+            3: np.eye(2),
+            4: rot_z(-90),
+            5: np.eye(2),
+            6: rot_z(180)
         }
-        monkeys[name] = monkey
-    return monkeys
+    }
+    position_map = {}   # New grid position to original grid position
+    # Face 1, 3, and 5 (3 is 4 on original)
+    for r in range(3*size):
+        for c in range(size):
+            position_map[(r, c+size)] = (r, c+2*size)
+    # Face 2 (3 on original)
+    for r in range(size):
+        for c in range(size):
+            position_map[(r+size, c)] = (r+size, c+size)
+    # Face 4 (6 on original)
+    original_face_6 = [[(r+2*size, c+3*size) for c in range(size)] for r in range(size)]
+    rotated_face_6 = rotate_matrix_counterclockwise(original_face_6)
+    for r in range(size):
+        for c in range(size):
+            position_map[(r+size, c+2*size)] = rotated_face_6[r][c]
+    # Face 6 (2 on original)
+    original_face_2 = [[(r+size, c) for c in range(size)] for r in range(size)]
+    rotated_face_2 = rotate_matrix_counterclockwise(original_face_2, count=2)
+    for r in range(size):
+        for c in range(size):
+            position_map[(r+3*size, c+size)] = rotated_face_2[r][c]
+    mapping['positions'] = position_map
+    return mapping
 
 
-def evaluate(values: [int, int], op: str):
-    a, b = values
-    string = f'{a} {op} {b}'
-    return int(eval(string))
+def get_net_from_test_grid(grid):
+    test_mapping = get_test_mapping()
+    new_grid = 2*np.ones((16, 12))
+    for new, old in test_mapping['positions'].items():
+        new_grid[new] = grid[old]
+    return new_grid, test_mapping
 
 
-def find_value(monkeys, monkey_name):
-    monkey = monkeys[monkey_name]
-    if monkey['value'] is not None:
-        return monkey['value']
-    children_values = [find_value(monkeys, child_name) for child_name in monkey['children']]
-    return evaluate(children_values, monkey['op'])
+'''Specific to Actual Input'''
+def get_mapping():
+    size = 50
+    mapping = {
+        'facing': {
+            1: np.eye(2),
+            2: rot_z(90),
+            3: np.eye(2),
+            4: rot_z(90),
+            5: np.eye(2),
+            6: rot_z(-90)
+        }
+    }
+    position_map = {}   # New grid position to original grid position
+    # Face 1, 3, and 5
+    for r in range(3*size):
+        for c in range(size):
+            position_map[(r, c+size)] = (r, c+size)
+    # Face 2
+    original_face_2 = [[(r+2*size, c) for c in range(size)] for r in range(size)]
+    rotated_face_2 = rotate_matrix_counterclockwise(original_face_2, count=3)
+    for r in range(size):
+        for c in range(size):
+            position_map[(r+size, c)] = rotated_face_2[r][c]
+    # Face 4 (6 on original)
+    original_face_4 = [[(r, c+2*size) for c in range(size)] for r in range(size)]
+    rotated_face_4 = rotate_matrix_counterclockwise(original_face_4, count=3)
+    for r in range(size):
+        for c in range(size):
+            position_map[(r+size, c+2*size)] = rotated_face_4[r][c]
+    # Face 6 (2 on original)
+    original_face_6 = [[(r+3*size, c) for c in range(size)] for r in range(size)]
+    rotated_face_6 = rotate_matrix_counterclockwise(original_face_6)
+    for r in range(size):
+        for c in range(size):
+            position_map[(r+3*size, c+size)] = rotated_face_6[r][c]
+    mapping['positions'] = position_map
+    return mapping
 
 
-def get_path(monkeys, desired_monkey, path_so_far):
-    monkey = monkeys[path_so_far[-1]]
-    if desired_monkey in monkey['children']:
-        return path_so_far + [desired_monkey], True
-    for child in monkey['children']:
-        new_path = path_so_far + [child]
-        desired_path, found = get_path(monkeys, desired_monkey, new_path)
-        if found:
-            return desired_path, True
-    return None, False
+def get_net_from_grid(grid):
+    mapping = get_mapping()
+    new_grid = 2*np.ones((4*50, 3*50))
+    for new, old in mapping['positions'].items():
+        new_grid[new] = grid[old]
+    return new_grid, mapping
+
+
+def determine_segment(grid, pos: (int, int), segment_size):
+    r, c = pos
+    rows, cols = grid.shape
+    if r < segment_size:
+        return 1
+    if r < 2*segment_size:
+        if c < segment_size:
+            return 2
+        if c < 2*segment_size:
+            return 3
+        if c < 3*segment_size:
+            return 4
+    if r < 3*segment_size:
+        return 5
+    return 6
+
+
+def get_next_pos(grid, pos, direction):
+    next_pos = (pos + direction)
+    next_direction = direction
+    if (np.array((0, 0)).T > next_pos).any() or (np.array(grid.shape).T <= next_pos).any() or grid[tuple(next_pos)] == 2:
+        rows, cols = grid.shape
+        size = int(cols/3)
+        current_segment = determine_segment(grid, pos, size)
+        if current_segment == 1:
+            print(1)
+            if (direction == np.array((0, -1))).all():
+                print('left')
+                next_pos = np.array((size, pos[0])).T
+                next_direction = np.array((1, 0)).T
+            elif (direction == np.array((0, 1))).all():
+                print('right')
+                next_pos = np.array((size, 3*size - pos[0] - 1)).T
+                next_direction = np.array((1, 0)).T
+            elif (direction == np.array((-1, 0))).all():
+                print('up')
+                next_pos = np.array((4*size-1, pos[1])).T
+                next_direction = np.array((-1, 0)).T
+        elif current_segment == 2:
+            print(2)
+            if (direction == np.array((-1, 0))).all():
+                print('up')
+                next_pos = np.array((pos[1], size)).T
+                next_direction = np.array((0, 1)).T
+            elif (direction == np.array((0, -1))).all():
+                print('left')
+                next_pos = np.array((pos[0], 3*size - 1)).T
+                next_direction = np.array((0, -1)).T
+            elif (direction == np.array((1, 0))).all():
+                print('down')
+                next_pos = np.array((3*size - 1 - pos[1], size)).T
+                next_direction = np.array((0, 1)).T
+        elif current_segment == 4:
+            print(4)
+            if (direction == np.array((-1, 0))).all():
+                print('up')
+                next_pos = np.array((3*size-1 - pos[1], 2*size-1)).T
+                next_direction = np.array((0, -1)).T
+            elif (direction == np.array((1, 0))).all():
+                print('down')
+                next_pos = np.array((pos[1], 2*size-1)).T
+                next_direction = np.array((0, -1)).T
+            elif (direction == np.array((0, 1))).all():
+                print('right')
+                next_pos = np.array((pos[0], 0)).T
+                next_direction = np.array((0, 1)).T
+        elif current_segment == 5:
+            print(5)
+            if (direction == np.array((0, 1))).all():
+                print('right')
+                next_pos = np.array((2*size-1, pos[0])).T
+                next_direction = np.array((-1, 0)).T
+            elif (direction == np.array((0, -1))).all():
+                print('left')
+                next_pos = np.array((2*size-1, 3*size - 1 - pos[0])).T
+                next_direction = np.array((-1, 0)).T
+        elif current_segment == 6:
+            print(6)
+            if (direction == np.array((0, 1))).all():
+                print('right')
+                next_pos = np.array((5*size-1-pos[0], 3*size-1)).T
+                next_direction = np.array((-1, 0)).T
+            elif (direction == np.array((0, -1))).all():
+                print('left')
+                next_pos = np.array((5*size-1-pos[0], 0)).T
+                next_direction = np.array((0, 1)).T
+            elif (direction == np.array((1, 0))).all():
+                print('down')
+                next_pos = np.array((0, pos[1])).T
+                next_direction = np.array((1, 0)).T
+    elif grid[tuple(next_pos)] == 0:
+        return next_pos, direction
+    if grid[tuple(next_pos)] == 1:
+        print('blocked')
+        return pos, direction
+    return next_pos, next_direction
+
+
+def draw_grid(grid):
+    for row in grid:
+        for value in row:
+            if value == 0:
+                print('.', end='')
+            if value == 1:
+                print('#', end='')
+            if value == 2:
+                print(' ', end='')
+        print()
+    print()
+
+
+def get_facing(direction):
+    if (direction == np.array((0, 1)).T).all():
+        return 0
+    if (direction == np.array((1, 0)).T).all():
+        return 1
+    if (direction == np.array((0, -1)).T).all():
+        return 2
+    if (direction == np.array((-1, 0)).T).all():
+        return 3
+    return None
+
+
+def validate():
+    grid, steps = get_input('input.txt')
+    grid, mapping = get_net_from_grid(grid)
+    draw_grid(grid)
 
 
 def main():
-    monkeys = get_input('input.txt')
-    path, found = get_path(monkeys, 'humn', ['root'])
-    child_not_in_path = [monkey for monkey in monkeys['root']['children'] if monkey != path[1]][0]
-    value = find_value(monkeys, child_not_in_path)
-
-    for ii in range(1, len(path)-1):
-        parent = path[ii]
-        child_in_path = path[ii+1]
-        child_not_in_path = monkeys[parent]['children'][1 - monkeys[parent]['children'].index(child_in_path)]
-        value_not_in_path = find_value(monkeys, child_not_in_path)
-        op = monkeys[parent]['op']
-        if op == '/':
-            if monkeys[parent]['children'].index(child_in_path) == 0:   # The one we're solving for
-                value = value * value_not_in_path
-            else:
-                value = int(value_not_in_path / value)
-        elif op == '+':
-            value = value - value_not_in_path
-        elif op == '-':
-            if monkeys[parent]['children'].index(child_in_path) == 0:  # The one we're solving for
-                value = value + value_not_in_path
-            else:
-                value = value_not_in_path - value
+    grid, steps = get_input('input.txt')
+    grid, mapping = get_net_from_grid(grid)
+    pos = np.array((0, 0)).T
+    rows, cols = grid.shape
+    for c in range(cols):
+        if grid[0, c] == 0:
+            pos = np.array((0, c)).T
+            break
+    direction = np.array((0, 1)).T
+    path = [tuple(pos)]
+    for step in steps:
+        if type(step) == int:
+            for _ in range(step):
+                pos, direction = get_next_pos(grid, pos, direction)
+                path.append(tuple(pos))
+        elif step == 'R':
+            direction = rot_z(-90) @ direction
+        elif step == 'L':
+            direction = rot_z(90) @ direction
         else:
-            value = int(value / value_not_in_path)
-    print(value)
+            print(f'unhandled step: {step}')
+    print()
+    mapped_position = np.array(mapping['positions'][tuple(pos)]) + np.array((1, 1))
+    print('position:', mapped_position)
+    final_segment = determine_segment(grid, pos, int(grid.shape[1]/3))
+    print('segment:', final_segment)
+    mapped_direction = mapping['facing'][final_segment]@direction
+    facing = get_facing(mapped_direction)
+    print('facing:', facing)
+    print('Direction:', direction)
+    print('mapped direction:', mapped_direction)
 
+    print()
+
+    a, b = mapped_position
+    print(1000*a + 4*b + facing)
+    # row, col = pos + np.array((1, 1)).T
+    # if (direction == np.array((0, 1)).T).all():
+    #     facing = 0
+    # if (direction == np.array((1, 0)).T).all():
+    #     facing = 1
+    # if (direction == np.array((0, -1)).T).all():
+    #     facing = 2
+    # if (direction == np.array((-1, 0)).T).all():
+    #     facing = 3
+    #
+    # print(row, col, facing)
+    # print(1000 * row + 4 * col + facing)
 
 main()
+
+# validate()
---
+++ 2022/Day22/Part2_hard_coded.py
@@ -0,0 +1,317 @@
+import numpy as np
+
+
+DIRECTIONS = {
+    'up': {
+        'vector': np.array((-1, 0), dtype='int'),
+        'L': 'left',
+        'R': 'right'
+    },
+    'down': {
+        'vector': np.array((1, 0), dtype='int'),
+        'L': 'right',
+        'R': 'left'
+    },
+    'left': {
+        'vector': np.array((0, -1), dtype='int'),
+        'L': 'down',
+        'R': 'up'
+    },
+    'right': {
+        'vector': np.array((0, 1), dtype='int'),
+        'L': 'up',
+        'R': 'down'
+    }
+}
+
+
+def rot_z(angle):
+    angle = np.pi * angle / 180
+    return np.array([
+        [np.cos(angle), -np.sin(angle)],
+        [np.sin(angle), np.cos(angle)]
+    ], dtype='int')
+
+
+def get_steps(word):
+    steps = []
+    number_str = ''
+    for char in word:
+        if char.isdigit():
+            number_str = number_str + char
+        else:
+            steps.append(int(number_str))
+            steps.append(char)
+            number_str = ''
+    if number_str.isdigit():
+        steps.append(int(number_str))
+    return steps
+
+
+def get_input(filename):
+    with open(filename) as file:
+        lines = file.read().splitlines()
+    grid = []
+    steps = get_steps(lines.pop())
+    lines.pop()
+    max_width = max([len(line) for line in lines])
+
+    for line in lines:
+        row = []
+        line = line + ' '*(max_width - len(line))
+        for char in line:
+            if char == '.':
+                row.append(0)
+            elif char == '#':
+                row.append(1)
+            elif char == ' ':
+                row.append(2)
+        grid.append(row)
+    return np.array(grid, dtype='int'), steps
+
+
+def determine_segment(grid, pos: (int, int), segment_size):
+    r, c = pos
+    rows, cols = grid.shape
+    if r < segment_size:
+        if c < 2*segment_size:
+            return 1
+        return 2
+    if r < 2*segment_size:
+        return 3
+    if r < 3*segment_size:
+        if c < segment_size:
+            return 4
+        return 5
+    return 6
+
+
+def get_next_pos(grid, pos, direction, file='actual'):
+    next_pos = tuple(np.array(pos) + DIRECTIONS[direction]['vector'])
+    _ = grid.shape
+    next_direction = direction
+    rows, cols = grid.shape
+    blocked = False
+    if next_pos[0] < 0 or next_pos[1] < 0 or next_pos[0] >= rows or next_pos[1] >= cols or grid[next_pos] == 2:
+        size = int(cols/3)
+        if file == 'actual':
+            current_segment = determine_segment(grid, pos, size)
+        else:
+            current_segment = determine_segment(grid, pos, size)
+        if current_segment == 1:
+            # print(1)
+            if direction == 'left': #(direction == np.array((0, -1))).all():
+                # print('left to 4')
+                next_pos = (3*size - 1 - pos[0], 0)
+                next_direction = 'right' #np.array((0, 1)).T
+            elif direction == 'up': #(direction == np.array((-1, 0))).all():
+                # print('up to 6')
+                next_pos = (2*size+pos[1], 0)
+                next_direction = 'right' #np.array((0, 1)).T
+        elif current_segment == 2:
+            # print(2)
+            if direction == 'up': #(direction == np.array((-1, 0))).all():
+                # print('up to 6')
+                next_pos = (4*size-1, pos[1] - 2*size)
+                next_direction = 'up' #np.array((-1, 0)).T
+            elif direction == 'right': #(direction == np.array((0, 1))).all():
+                # print('right to 5')
+                next_pos = (3*size - 1 - pos[0], 2*size - 1)
+                next_direction = 'left' #np.array((0, -1)).T
+            elif direction == 'down': #(direction == np.array((1, 0))).all():
+                # print('down to 3')
+                next_pos = (pos[1] - size, 2*size - 1)
+                next_direction = 'left' #np.array((0, -1)).T
+        elif current_segment == 3:
+            # print(3)
+            if direction == 'left': #(direction == np.array((0, -1))).all():
+                # print('left to 4')
+                next_pos = (2*size, pos[0]-size)
+                next_direction = 'down' # np.array((1, 0)).T
+            elif direction == 'right': # '(direction == np.array((0, 1))).all():
+                # print('right to 2')
+                next_pos = (size-1, pos[0]+size)
+                next_direction = 'up' # np.array((-1, 0)).T
+        elif current_segment == 4:
+            # print(4)
+            if direction == 'up': # '(direction == np.array((-1, 0))).all():
+                # print('up to 3')
+                next_pos = (pos[1]+5, size)
+                next_direction = 'right' # np.array((0, 1)).T
+            elif direction == 'left': # '(direction == np.array((0, -1))).all():
+                # print('left to 1')
+                next_pos = (3*size-1 - pos[0], size)
+                next_direction = 'right' # np.array((0, 1)).T
+        elif current_segment == 5:
+            # print(5)
+            if direction == 'right': # '(direction == np.array((0, 1))).all():
+                # print('right to 2')
+                next_pos = (3*size-1 - pos[0], 3*size-1)
+                next_direction = 'left' # np.array((0, -1)).T
+            elif direction == 'down': # '(direction == np.array((1, 0))).all():
+                # print('down to 6')
+                next_pos = (2*size + pos[1], size-1)
+                next_direction = 'left' # np.array((0, -1)).T
+        elif current_segment == 6:
+            # print(6)
+            if direction == 'right': # '(direction == np.array((0, 1))).all():
+                # print('right to 5')
+                next_pos = (3*size-1, pos[0]-2*size)
+                next_direction = 'up' # np.array((-1, 0)).T
+            elif direction == 'down': # '(direction == np.array((1, 0))).all():
+                # print('down to 2')
+                next_pos = (0, pos[1] + 2*size)
+                next_direction = 'down' # np.array((1, 0)).T
+            elif direction == 'left': # '(direction == np.array((0, -1))).all():
+                # print('left to 1')
+                next_pos = (0, pos[0] - 2*size)
+                next_direction = 'down' # np.array((1, 0)).T
+    elif grid[next_pos] == 0:
+        return next_pos, direction, blocked
+    if grid[next_pos] == 1:
+        blocked = True
+        return pos, direction, blocked
+    return next_pos, next_direction, blocked
+
+
+def draw_grid(grid):
+    for row in grid:
+        for value in row:
+            if value == 0:
+                print('.', end='')
+            if value == 1:
+                print('#', end='')
+            if value == 2:
+                print(' ', end='')
+        print()
+    print()
+
+
+def get_facing(direction):
+    if direction == 'right': #(direction == np.array((0, 1)).T).all():
+        return 0
+    if direction == 'down': #'(direction == np.array((1, 0)).T).all():
+        return 1
+    if direction == 'left': #'(direction == np.array((0, -1)).T).all():
+        return 2
+    if direction == 'up': #'(direction == np.array((-1, 0)).T).all():
+        return 3
+    return None
+
+
+def validate():
+    grid, steps = get_input('test_input_2.txt')
+
+    # print(get_next_pos(grid, (0, 14), 'right'))
+    print()
+    print('Running Validation...')
+    expected = {
+        1: {
+            ((4, 5), 'left'): ((10, 0), 'right', False),
+            ((0, 5), 'left'): ((14, 0), 'right', False),
+            ((0, 5),   'up'): ((15, 0), 'right', False),
+            ((0, 9),   'up'): ((19, 0), 'right', False),
+            ((4, 6), 'down'): ((5, 6), 'down', False),
+            ((1, 6),   'up'): ((0, 6), 'up', False),
+            ((3, 5), 'left'): ((3, 5), 'left', True),
+            ((0, 8),   'up'): ((0, 8), 'up', True),
+        },
+        2: {
+            ((0, 10), 'up'): ((19, 0), 'up', False),
+            ((0, 14), 'up'): ((19, 4), 'up', False),
+            ((0, 14), 'right'): ((14, 9), 'left', False),
+            ((4, 14), 'right'): ((10, 9), 'left', False),
+            ((4, 14), 'down'): ((9, 9), 'left', False),
+            ((4, 10), 'down'): ((5, 9), 'left', False),
+            ((0, 11), 'up'): ((0, 11), 'up', True),
+            ((3, 14), 'right'): ((3, 14), 'right', True),
+            ((4, 13), 'down'): ((4, 13), 'down', True),
+        },
+        3: {
+            ((5,  5), 'left'): ((10, 0), 'down', False),
+            ((9,  5), 'left'): ((10, 4), 'down', False),
+            ((9, 9), 'right'): ((4, 14), 'up', False),
+            ((5, 9), 'right'): ((4, 10), 'up', False),
+            ((7, 5), 'left'): ((7,  5), 'left', True),
+            ((7, 9), 'right'): ((7, 9), 'right', True),
+        },
+        4: {
+            ((14, 0), 'left'): ((0, 5), 'right', False),
+            ((10, 0), 'left'): ((4, 5), 'right', False),
+            ((10, 0), 'up'): ((5, 5), 'right', False),
+            ((10, 4), 'up'): ((9, 5), 'right', False),
+            ((12, 0), 'left'): ((12, 0), 'left', True),
+            ((10, 3), 'up'): ((10, 3), 'up', True),
+        },
+        5: {
+            ((10, 9), 'right'): ((4, 14), 'left', False),
+            ((14, 9), 'right'): ((0, 14), 'left', False),
+            ((14, 9), 'down'): ((19, 4), 'left', False),
+            ((14, 5), 'down'): ((15, 4), 'left', False),
+            ((12, 9), 'right'): ((12, 9), 'right', True),
+            ((14, 8), 'down'): ((14, 8), 'down', True),
+        },
+        6: {
+            ((15, 0), 'left'): ((0, 5), 'down', False),
+            ((19, 0), 'left'): ((0, 9), 'down', False),
+            ((19, 0), 'down'): ((0, 10), 'down', False),
+            ((19, 4), 'down'): ((0, 14), 'down', False),
+            ((19, 4), 'right'): ((14, 9), 'up', False),
+            ((15, 4), 'right'): ((14, 5), 'up', False),
+            ((17, 0), 'left'): ((17, 0), 'left', True),
+            ((19, 2), 'down'): ((19, 2), 'down', True),
+            ((17, 4), 'right'): ((17, 4), 'right', True),
+        },
+
+    }
+    for face, test in expected.items():
+        print()
+        print(f'Face {face}:')
+        for state, expected_output in test.items():
+            pos = state[0]
+            direction = state[1]
+            output = get_next_pos(grid, pos, direction)
+            if output != expected_output:
+                print(f'\tFailed')
+                print(f'\t\t   Input: {state}')
+                print(f'\t\tExpected: {expected_output}')
+                print(f'\t\t  Actual: {output}')
+                return
+            else:
+                print(f'\tPass: {state} -> {output}')
+
+
+
+
+def main():
+    grid, steps = get_input('input.txt')
+    pos = np.array((0, 0)).T
+    _, cols = grid.shape
+    for c in range(cols):
+        if grid[0, c] == 0:
+            pos = np.array((0, c)).T
+            break
+    print(pos)
+    direction = 'right' #np.array((0, 1)).T
+
+    # _ = input(pos)
+    for step in steps:
+        if type(step) == int:
+            for _ in range(step):
+                pos, direction, blocked = get_next_pos(grid, pos, direction)
+                if blocked:
+                    break
+                # print(pos, direction)
+        elif step in 'LR':
+            direction = DIRECTIONS[direction][step] #rot_z(-90) @ direction
+        else:
+            print(f'unhandled step: {step}')
+    print()
+    a, b = np.array(pos) + np.ones(np.array(pos).shape, dtype='int')
+    facing = get_facing(direction)
+    print(a, b, facing)
+    print(1000*a + 4*b + facing)
+
+
+# validate()
+main()
