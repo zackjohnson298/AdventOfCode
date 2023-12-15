c0cfa10c8871cfaaee39d931e5a34ba11038be2d
zackjohnson298 <zackjohnson298@gmail.com>
2023-12-15 14:56:40 -0700

    Day 12 Part 2 complete

--- 2023/Day12/Part2.py
+++ 2023/Day12/Part2.py
@@ -1,67 +1,57 @@
 from typing import List, Tuple, Dict
-import numpy as np
+import functools
 
 
-def get_input(filename) -> List[Tuple[str, List[int]]]:
+def get_input(filename) -> List[Tuple[str, Tuple[int]]]:
     with open(filename) as file:
         lines = file.read().splitlines()
     output = []
     for line in lines:
         springs, rule = line.split()
-        output.append((springs, [int(char) for char in rule.split(',')]))
+        output.append((springs, tuple([int(char) for char in rule.split(',')])))
     return output
 
 
-def validate_springs(springs: str, rules: List[int]):
-    current = ''
-    groups = []
-    for char in springs:
-        if char == '#':
-            current = current + char
-        elif current:
-            groups.append(current)
-            current = ''
-    if current:
-        groups.append(current)
-    if len(groups) == len(rules):
-        return all([len(group) == rule for group, rule in zip(groups, rules)])
-    return False
-
-
-def find_arrangements(springs: str, rules: List[int]):
-    total = 0
-    max_count = 2**springs.count('?')
-    total_binary_len = len(bin(max_count-1)[2:])
-    # print(springs)
-    print(max_count)
-    for ii in range(max_count):
-        qm_count = 0
-        bin_str = bin(ii)[2:]
-        bin_str = '0'*(total_binary_len - len(bin_str)) + bin_str
-        new_str = ''
-        for char in springs:
-            if char == '?':
-                new_str = new_str + ('#' if int(bin_str[qm_count]) else '.')
-                qm_count += 1
-            else:
-                new_str = new_str + char
-        if validate_springs(new_str, rules):
-            total += 1
-    return total
-
-
-def unfold(springs, rules, count=5) -> Tuple[str, List[int]]:
+def unfold(springs: str, rules: Tuple[int], count=5) -> Tuple[str, Tuple[int]]:
     new_rules = rules*count
     new_springs = '?'.join(springs for _ in range(count))
     return new_springs, new_rules
 
 
+@functools.cache
+def find_arrangements(springs: str, rules: Tuple[int]) -> int:
+    if len(springs) == 0 and len(rules) != 0:
+        return 0
+    elif len(rules) == 0:
+        return 1 if '#' not in springs else 0
+    char = springs[0]
+    rule = rules[0]
+    assert char in '.#?'
+    if rule > len(springs) or len(rules) > 2*len(springs):
+        return 0
+    elif char == '#':
+        current_group = springs[:rule]
+        if not all([c in '#?' for c in current_group]):
+            return 0
+        if len(springs) == rule:
+            return 1 if len(rules) == 1 else 0
+        if springs[rule] in '.?':
+            return find_arrangements(springs[rule+1:], rules[1:])
+        return 0
+    elif char == '.':
+        return find_arrangements(springs[1:], rules)
+    else:
+        dot_spring = '.' + springs[1:]
+        pound_spring = '#' + springs[1:]
+        return find_arrangements(dot_spring, rules) + find_arrangements(pound_spring, rules)
+
+
 def main():
-    springs = get_input('input.txt')
+    data = get_input('input.txt')
     total = 0
-    for spring, rules in springs:
-        new = find_arrangements(*unfold(spring, rules))
-        print(spring, new)
+    for springs, rules in data:
+        new = find_arrangements(*unfold(springs, rules))
+        # print(springs, new)
         total += new
     print(total)
 
