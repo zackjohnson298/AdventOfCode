a76e4740338403aa36560dcc4f2df55a9b695f2d
zackjohnson298 <zackjohnson298@gmail.com>
2023-12-17 23:10:28 -0700

    Day 18 Complete

--- 2023/Day18/Part1.py
+++ 2023/Day18/Part1.py
@@ -13,57 +13,83 @@ def get_input(filename) -> List[Tuple[str, int, str]]:
     return output
 
 
-def neighbors(pos: Tuple[int, int], width: int, height: int) -> List[Tuple[int, int]]:
-    output = []
-    r, c = pos
-    for nr in [r - 1, r, r + 1]:
-        for nc in [c - 1, c, c + 1]:
-            if (nr, nc) != pos and 0 <= nr < height and 0 <= nc < width:
-                output.append((nr, nc))
-    return output
+def get_area(vertices: List[Tuple[int, int]]) -> int:
+    total = 0
+    for ii in range(1, len(vertices)):
+        x1, y1 = vertices[ii-1]
+        x2, y2 = vertices[ii]
+        total += (x1*y2 - y1*x2)
+    total /= 2
+    return abs(total)
 
 
 def main():
     instructions = get_input('input.txt')
     directions = {'R': (0, 1), 'L': (0, -1), 'U': (-1, 0), 'D': (1, 0)}
     points = [(0, 0)]
-    for direction, value, color in instructions:
+    perimeter = 0
+    for direction, value, _ in instructions:
         r, c = points[-1]
+        perimeter += value
         dr, dc = directions[direction]
-        new_points = [(r + ii*dr, c + ii*dc) for ii in range(1, value+1)]
-        points.extend(new_points)
-    min_r = min([point[0] for point in points])
-    min_c = min([point[1] for point in points])
-    for ii in range(len(points)):
-        point = points[ii]
-        point = (point[0] - (min_r - 2), point[1] - (min_c - 2))
-        points[ii] = point
-
-    height = max([point[0] for point in points]) + 2
-    width = max([point[1] for point in points]) + 2
-    visited_grid = [[False for _ in range(width)] for _ in range(height)]
-
-    iterations = 0
-    queue = [(0, 0)]
-    while queue:
-        if iterations % 10 == 0:
-            print(f'Queue: {len(queue)},\tIter: {iterations}')
-        iterations += 1
-        r, c = queue.pop(0)
-        visited_grid[r][c] = True
-        for neighbor in neighbors((r, c), width, height):
-            nr, nc = neighbor
-            if not visited_grid[nr][nc] and neighbor not in points + queue:
-                queue.append(neighbor)
-
-    for r in range(height):
-        line = ''
-        for c in range(width):
-            line = line + ('#' if (r, c) in points else 'o' if visited_grid[r][c] else '.')
-        print(line)
-
-    print('Counting enclosed positions')
-    print(sum([sum([not value for value in row]) for row in visited_grid]))
+        new_point = (r + value*dr, c + value*dc)
+        points.append(new_point)
+    area = get_area(points) + (perimeter - 2)/2 + 2
+    print(area)
 
 
 main()
+
+
+#  Flood Fil Method
+
+# def neighbors(pos: Tuple[int, int], width: int, height: int) -> List[Tuple[int, int]]:
+#     output = []
+#     r, c = pos
+#     for nr in [r - 1, r, r + 1]:
+#         for nc in [c - 1, c, c + 1]:
+#             if (nr, nc) != pos and 0 <= nr < height and 0 <= nc < width:
+#                 output.append((nr, nc))
+#     return output
+
+# def main():
+#     instructions = get_input('input.txt')
+#     directions = {'R': (0, 1), 'L': (0, -1), 'U': (-1, 0), 'D': (1, 0)}
+#     points = [(0, 0)]
+#     for direction, value, color in instructions:
+#         r, c = points[-1]
+#         dr, dc = directions[direction]
+#         new_points = [(r + ii*dr, c + ii*dc) for ii in range(1, value+1)]
+#         points.extend(new_points)
+#     min_r = min([point[0] for point in points])
+#     min_c = min([point[1] for point in points])
+#     for ii in range(len(points)):
+#         point = points[ii]
+#         point = (point[0] - (min_r - 2), point[1] - (min_c - 2))
+#         points[ii] = point
+#
+#     height = max([point[0] for point in points]) + 2
+#     width = max([point[1] for point in points]) + 2
+#     visited_grid = [[False for _ in range(width)] for _ in range(height)]
+#
+#     iterations = 0
+#     queue = [(0, 0)]
+#     while queue:
+#         if iterations % 10 == 0:
+#             print(f'Queue: {len(queue)},\tIter: {iterations}')
+#         iterations += 1
+#         r, c = queue.pop(0)
+#         visited_grid[r][c] = True
+#         for neighbor in neighbors((r, c), width, height):
+#             nr, nc = neighbor
+#             if not visited_grid[nr][nc] and neighbor not in points + queue:
+#                 queue.append(neighbor)
+#
+#     for r in range(height):
+#         line = ''
+#         for c in range(width):
+#             line = line + ('#' if (r, c) in points else 'o' if visited_grid[r][c] else '.')
+#         print(line)
+#
+#     print('Counting enclosed positions')
+#     print(sum([sum([not value for value in row]) for row in visited_grid]))
--- 2023/Day18/Part2.py
+++ 2023/Day18/Part2.py
@@ -1,70 +1,44 @@
 from typing import List, Tuple, Dict, Optional, Set
 
 
-def get_input(filename) -> List[Tuple[str, int, str]]:
+def get_input(filename) -> List[Tuple[str, int]]:
     with open(filename) as file:
         lines = file.read().splitlines()
     output = []
+    directions = 'RDLU'
     for line in lines:
-        direction, value_str, color_str = line.split()
-        color_str = color_str.replace(')', '')
-        color_str = color_str.replace('(', '')
-        output.append((direction, int(value_str), color_str))
+        instruction_str = line.split()[-1]
+        instruction_str = instruction_str.replace(')', '')
+        instruction_str = instruction_str.replace('(#', '')
+        value = int(instruction_str[:-1], 16)
+        direction = directions[int(instruction_str[-1])]
+        output.append((direction, value))
     return output
 
 
-def neighbors(pos: Tuple[int, int], width: int, height: int) -> List[Tuple[int, int]]:
-    output = []
-    r, c = pos
-    for nr in [r - 1, r, r + 1]:
-        for nc in [c - 1, c, c + 1]:
-            if (nr, nc) != pos and 0 <= nr < height and 0 <= nc < width:
-                output.append((nr, nc))
-    return output
+def get_area(vertices: List[Tuple[int, int]]) -> int:
+    total = 0
+    for ii in range(1, len(vertices)):
+        x1, y1 = vertices[ii-1]
+        x2, y2 = vertices[ii]
+        total += (x1*y2 - y1*x2)
+    total /= 2
+    return abs(total)
 
 
 def main():
     instructions = get_input('input.txt')
     directions = {'R': (0, 1), 'L': (0, -1), 'U': (-1, 0), 'D': (1, 0)}
     points = [(0, 0)]
-    for direction, value, color in instructions:
+    perimeter = 0
+    for direction, value in instructions:
         r, c = points[-1]
+        perimeter += value
         dr, dc = directions[direction]
-        new_points = [(r + ii*dr, c + ii*dc) for ii in range(1, value+1)]
-        points.extend(new_points)
-    min_r = min([point[0] for point in points])
-    min_c = min([point[1] for point in points])
-    for ii in range(len(points)):
-        point = points[ii]
-        point = (point[0] - (min_r - 2), point[1] - (min_c - 2))
-        points[ii] = point
+        new_point = (r + value*dr, c + value*dc)
+        points.append(new_point)
+    area = get_area(points) + (perimeter - 2)/2 + 2
+    print(area)
 
-    height = max([point[0] for point in points]) + 2
-    width = max([point[1] for point in points]) + 2
-    visited_grid = [[False for _ in range(width)] for _ in range(height)]
-
-    iterations = 0
-    queue = [(0, 0)]
-    while queue:
-        if iterations % 10 == 0:
-            print(f'Queue: {len(queue)},\tIter: {iterations}')
-        iterations += 1
-        r, c = queue.pop(0)
-        visited_grid[r][c] = True
-        for neighbor in neighbors((r, c), width, height):
-            nr, nc = neighbor
-            if not visited_grid[nr][nc] and neighbor not in points + queue:
-                queue.append(neighbor)
-
-    for r in range(height):
-        line = ''
-        for c in range(width):
-            line = line + ('#' if (r, c) in points else 'o' if visited_grid[r][c] else '.')
-        print(line)
-
-    print('Counting enclosed positions')
-    print(sum([sum([not value for value in row]) for row in visited_grid]))
-    # print()
-    # print(max_r-min_r, max_c-min_c)
 
 main()
